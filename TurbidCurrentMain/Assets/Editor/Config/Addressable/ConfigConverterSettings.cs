434c3722014c37b9b8980e8913b05be6c6594c2d
Dean <mengzi74110@163.com>
2021-12-30 17:36:35 +0800

    Addressable

---
+++ TurbidCurrentMain/Assets/Editor/Config/Addressable/ConfigConverterSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+
+public class ConfigConverterSettings : ScriptableObject
+{
+    public static AddressableAssetSettings CurSettings => AddressableAssetSettingsDefaultObject.Settings;
+
+
+    [MenuItem("CustomToolbar/Addressable/Clear Groups")]
+    static void ClearGroups()
+    {
+        List<string> guids = new List<string>();
+        //只移除groups下面的条目，不移除groups。（在Mac下会出Groups设置 莫名其妙重置的bug）
+        foreach (var group in CurSettings.groups)
+        {
+            guids.Clear();
+            foreach (var item in group.entries)
+            {
+                guids.Add(item.guid);
+            }
+        }
+        foreach (var item in guids)
+        {
+            CurSettings.RemoveAssetEntry(item);
+        }
+        Debug.Log("Clear AddressableGroups Entries Done!");
+    }
+}
