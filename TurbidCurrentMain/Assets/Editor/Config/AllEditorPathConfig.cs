c3b4dfe18ffb06c2418bece27d48501c35273abe
Dean <mengzi74110@163.com>
2021-12-31 17:12:25 +0800

    AllEditorPathConfig.cs

--- TurbidCurrentMain/Assets/Editor/Config/AllEditorPathConfig.cs
+++ TurbidCurrentMain/Assets/Editor/Config/AllEditorPathConfig.cs
@@ -5,4 +5,8 @@ using UnityEngine;
 public static class AllEditorPathConfig
 {
     public static string ScriptsPath = "Assets/MainProject/Scripts";
+    public static string TargetTblFolder = "Assets/MainProject/Config/Tbl/";
+    public const string TargetAssetFolder = "Assets/MainProject/Config/Asset/";
+    public static string CSharpFolder = "Assets/MainProject/Scripts/Config/Define/";
+    public const string LuaFolder = "Assets/MainProject/Scripts/Config/LuaScripts/";
 }
