09184bbe3fbf169a82c84e8d7665b472b9ac3543
Dean <mengzi74110@163.com>
2021-12-31 11:51:50 +0800

    添加方法Convert To Utf-8

---
+++ TurbidCurrentMain/Assets/Editor/Config/Addressable/ConfigFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//将Excel 转换为 CSV，再转换为CS或者Lua的基类
+public abstract class ConfigFile
+{
+    protected string m_fileName;                    // 文件名
+    protected string[] m_fieldNames;                // 变量名
+    protected string[] m_types;                     // 类型名
+    protected string[] m_annotations;               // 注释
+    protected string[][] m_valueLines;              // 值
+
+    public interface IDecoder
+    {
+        void DecodeFile(string folder, string fileName, out string[] fieldNames, out string[] types, out string[] annotations, out string[][] valueLines);
+    }
+}
