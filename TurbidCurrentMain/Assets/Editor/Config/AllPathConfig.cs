3103ee7935877c2efcbc59ad2b79db6e4ba70365
Dean <mengzi74110@163.com>
2021-12-31 11:54:20 +0800

    AllPathConfig.cs

---
+++ TurbidCurrentMain/Assets/Editor/Config/AllPathConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AllPathConfig 
+{
+    public static string ScriptsPath = "Assets/MainProject/Scripts";
+}
