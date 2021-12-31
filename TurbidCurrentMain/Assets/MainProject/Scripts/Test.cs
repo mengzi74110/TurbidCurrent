1fe52b30b697558cf654afffdffefd4085f441e7
Dean <mengzi74110@163.com>
2021-12-31 09:53:01 +0800

    test.cs

---
+++ TurbidCurrentMain/Assets/MainProject/Scripts/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+       
+    }
+
+ 
+    void Update()
+    {
+       
+    }
+}
