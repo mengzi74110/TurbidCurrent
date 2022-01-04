0cbdadedbb16965660bbeea7cc2036893c3bcfca
Dean <mengzi74110@163.com>
2022-01-04 09:40:26 +0800

    Add RemoveGroups

--- TurbidCurrentMain/Assets/Editor/Config/Addressable/ConfigConverterSettings.cs
+++ TurbidCurrentMain/Assets/Editor/Config/Addressable/ConfigConverterSettings.cs
@@ -14,7 +14,7 @@ public class ConfigConverterSettings : ScriptableObject
     static void ClearGroups()
     {
         List<string> guids = new List<string>();
-        //只移除groups下面的条目，不移除groups。（在Mac下会出Groups设置 莫名其妙重置的bug）
+        //只移除groups下面的条目，不移除groups。（在Mac下，移除Groups以后，Groups的设置 莫名其妙重置的bug）
         foreach (var group in CurSettings.groups)
         {
             guids.Clear();
@@ -28,6 +28,12 @@ public class ConfigConverterSettings : ScriptableObject
             CurSettings.RemoveAssetEntry(item);
         }
         Debug.Log("Clear AddressableGroups Entries Done!");
+        //移除Groups
+        //var listGroups = CurSettings.groups;
+        //for (int i = 0; i < listGroups.Count; i++)
+        //{
+        //    CurSettings.RemoveGroup(listGroups[i]);
+        //}
     }
 
     [MenuItem("CustomToolbar/Addressable/Reset Groups")]
