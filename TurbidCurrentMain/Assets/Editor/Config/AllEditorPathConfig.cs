fde18919668172cfead19758806820025b9e1d65
Dean <mengzi74110@163.com>
2022-01-06 09:37:46 +0800

    AllEditorPathConfig.cs

--- TurbidCurrentMain/Assets/Editor/Config/AllEditorPathConfig.cs
+++ TurbidCurrentMain/Assets/Editor/Config/AllEditorPathConfig.cs
@@ -12,14 +12,14 @@ public static class AllEditorPathConfig
     //Excel转换配置文件路径；
     public static string SettingFilePath = "Assets/Editor/Config/ConfigConvert/ConfigConverterSettings_Config.csv";
 
+    //Excel转换为txt路径
+    public static string TargetTblFolder = "Assets/MainProject/BuildAssets/Config/Tbl/";
     //脚本文件路径
     public static string ScriptsPath = "Assets/MainProject/Scripts";
-    //Excel转换为txt路径
-    public static string TargetTblFolder = "Assets/MainProject/Config/Tbl/";
     //Excel转换生成 Class文件路径
     public static string CSharpFolder = "Assets/MainProject/Scripts/Config/Define/";
     //Excel转换生成Lua文件路径
     public const string LuaFolder = "Assets/MainProject/Scripts/Config/LuaScripts/";
 
-    public const string TargetAssetFolder = "Assets/MainProject/Config/Asset/";
+    public const string TargetAssetFolder = "Assets/MainProject/BuildAssets/Config/Asset/";
 }
