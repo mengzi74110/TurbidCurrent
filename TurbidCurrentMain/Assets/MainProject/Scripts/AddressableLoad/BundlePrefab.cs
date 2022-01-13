dbb611a28b19a05b97fb2840bc64470fe4d5f9f3
Dean <mengzi74110@163.com>
2022-01-13 10:51:52 +0800

    取消一些方法

--- TurbidCurrentMain/Assets/MainProject/Scripts/AddressableLoad/BundlePrefab.cs
+++ TurbidCurrentMain/Assets/MainProject/Scripts/AddressableLoad/BundlePrefab.cs
@@ -4,20 +4,6 @@ namespace Common
     public class BundlePrefab : BundleNormal<GameObject>
     {
         public BundlePrefab(string path) : base(path) { }
-
-
-        public GameObject CloneObj()
-        {
-            return UnityEngine.Object.Instantiate(AssetObj, null);
-        }
-
-        public GameObject CloneObj(Transform parentTrans)
-        {
-            return UnityEngine.Object.Instantiate(AssetObj, parentTrans);
-        }
-        public GameObject CloneObj(Transform parentTrans,Vector3 pos,Quaternion qua)
-        {
-            return UnityEngine.Object.Instantiate(AssetObj, pos,qua,parentTrans);
-        }
+  
     }  
 }
