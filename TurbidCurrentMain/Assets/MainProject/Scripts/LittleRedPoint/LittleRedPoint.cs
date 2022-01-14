b4459fdbaf82d1b841f13417e58daabfd5475b3b
Dean <mengzi74110@163.com>
2022-01-14 19:18:56 +0800

    LittleRedPoint folder

---
+++ TurbidCurrentMain/Assets/MainProject/Scripts/LittleRedPoint/LittleRedPoint.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using UnityEngine;
+
+public class LittleRedPoint
+{
+    Func<bool> m_condition;
+}
---
+++ TurbidCurrentMain/Assets/MainProject/Scripts/LittleRedPoint/LittleRedPointUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Common;
+namespace TurbidCurrent
+{
+    public class LittleRedPointUI : UIBase
+    {
+        [SerializeField]
+        Sprite m_RedIcon;
+
+        Transform m_parent;
+
+        public bool IsShow
+        {
+            get
+            {
+                return ActiveSelf;
+            }
+            set
+            {
+                ActiveSelf = value;
+            }
+        }
+        protected override void OnAwake()
+        {
+            
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void SetScreenFlag()
+        {
+            
+        }
+
+        protected override void SetUIFlag()
+        {
+            
+        }
+
+        //加载小红点模板
+        public static void LoadPrefab()
+        {
+            BundleUIPrefab redPointTemplate = new BundleUIPrefab("", "");
+        }
+    }
+}
