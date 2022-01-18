50b2d7dde2a9ba18e999df0d84d240e6db5ec441
Dean <mengzi74110@163.com>
2022-01-18 09:07:40 +0800

    Scripts push

--- TurbidCurrentMain/Assets/MainProject/Scripts/GlobalData/UIData.cs
+++ TurbidCurrentMain/Assets/MainProject/Scripts/GlobalData/UIData.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace TurbidCurrent
 {
+    //需要与预制体的名字相同；
     public enum UIFlag
     {
         None,
         RedPoint,
         UI_LogoWnd,
+        UI_LoadingPanel,
     }
     public enum ScreenFlag
     {
